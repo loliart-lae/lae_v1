051367e50633131a79fa50dcb497e22699e4192b
iVampireSP <im@ivampiresp.com>
2021-09-09 15:21:24 +0800

    Update billing.php

--- config/billing.php
+++ config/billing.php
@@ -4,7 +4,7 @@ return [
     'mid' => 3561,
     'key' => 'z2v6hb9e7saymcbp1lfi0fc1wd9c',
     'api_url' => 'https://www.3mpay.com/createOrder',
-    'notify' => 'http://lightappengine.test/dashboard/billing/notify',
-    'return' => 'http://lightappengine.test/dashboard/billing/return',
+    'notify' => env('APP_URL') . '/dashboard/billing/notify',
+    'return' => env('APP_URL') . '/dashboard/billing/return',
     'exchange_rate' => 100
 ];
