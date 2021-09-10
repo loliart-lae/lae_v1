d953e2e8abc9e82bdea575894a3b9da858bbeada
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-10 23:12:27 +0800

    字符修改

--- resources/views/lxd/create.blade.php
+++ resources/views/lxd/create.blade.php
@@ -46,7 +46,7 @@
         <br />
         <br />
         <span class="mdui-typo-headline">选择地区服务器</span>
-        <p class="mdui-typo-subheading">地区服务器影响着访问速度以及连通性,稳定性,以及基础价格。</p>
+        <p class="mdui-typo-subheading">地区服务器影响着访问速度以及连通性，稳定性，以及基础价格。</p>
         <br />
 
         <div class="mdui-table-fluid">
