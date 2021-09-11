6bba8afa957f60cea864cfdd7cdd0259bf4a0b4d
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-11 17:53:14 +0800

    对齐

--- resources/views/contributes.blade.php
+++ resources/views/contributes.blade.php
@@ -11,9 +11,10 @@
     <div>
         <div class="mdui-typo-display-2">开发人员</div>
         <div style="margin-left: 10px;padding: 20px">
+        
         <div class="mdui-col-sm-6 mdui-col-md-4">
-        <div class="mdui-card">
-            <!-- 卡片头部，包含头像、标题、副标题 -->
+            <div class="mdui-card">
+                <!-- 卡片头部，包含头像、标题、副标题 -->
                 <div class="mdui-card-header">
                     <img class="mdui-card-header-avatar" src="https://nwl.im/avatar"/>
                 </div>
