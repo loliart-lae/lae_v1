a6c890fef65d40bc56a916f75b79ab4f948dad37
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-12 01:15:29 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,7 @@
         </div>
     </div>
     <div class="mdui-container mdui-p-a-2 pjax-container">
-        <a id="pre_btn" href="{{ url()->previous() }}" class="mdui-btn mdui-ripple mdui-m-b-1"><i
+        <a id="pre_btn" href="{{ url()->previous() }}" class="mdui-btn mdui-ripple mdui-m-b-2"><i
             style="position: relative; top: -1px;margin-right: 2px"
             class="mdui-icon material-icons">arrow_back</i>返回</a>
 
