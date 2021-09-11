0f29a1eed468c99bb242b85cdcbde3fddb545e38
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-12 01:12:32 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
 
         @yield('content')
 
-        <div class="mdui-typo">
+        <div class="mdui-typo margin-top">
             <p class="mdui-typo-caption-opacity mdui-text-center">
                 <a href="{{ route('contributes') }}">Contributors</a>
                 <br />Light App Engine<br />
