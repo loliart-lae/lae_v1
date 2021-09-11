dafa06ade254d6aa12424aafc7dee8f350a95d0f
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-12 00:58:08 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,13 +80,11 @@
 
         @yield('content')
 
-        <div class="mdui-typo">
-            <p class="mdui-typo-caption-opacity mdui-text-center">
-                <a href="{{ route('contributes') }}">Contributors</a>
-                <br />Light App Engine<br />
-                Crafted with 💗 by iVampireSP.com<br />
-            </p>
-        </div>
+        <p class="mdui-typo-caption-opacity mdui-text-center">
+            <a href="{{ route('contributes') }}">Contributors</a>
+            <br />Light App Engine<br />
+            Crafted with 💗 by iVampireSP.com<br />
+        </p>
     </div>
 
     
