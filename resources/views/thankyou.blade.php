27d0dc6ba153ac8e01fd5ea755ace05f6488af1d
iVampireSP.com <im@ivampiresp.com>
2021-09-11 14:50:20 +0800

    修改充值页面

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -5,6 +5,6 @@
 @section('content')
 
 <div class="mdui-typo-display-2">充值成功</div><br>
-<div class="mdui-typo-display-1-opacity">感谢你支持 LAE 轻量应用引擎</div>
+<div class="mdui-typo-display-1-opacity">感谢您支持 LAE 轻量应用引擎。</div>
 
 @endsection
