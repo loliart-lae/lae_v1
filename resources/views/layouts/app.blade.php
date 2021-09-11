6744bf5d995e0024ed52c65e8172a6c47e973f4e
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-11 21:19:36 +0800

    美观调整

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,21 +43,21 @@
         <div class="mdui-tab mdui-color-theme mdui-tab-scrollable mdui-tab-full-width" mdui-tab>
             @guest
                 <a href="{{ route('index') }}" class="mdui-ripple mdui-ripple-white">Light App Engine</a>
+                <a href="{{ route('login') }}" class="mdui-btn mdui-ripple mdui-ripple-white">登录</a>
                 <a target="_blank" href="https://f.lightart.top/t/knowledge-base"
                     class="mdui-btn mdui-ripple mdui-ripple-white">知识库</a>
                 <a href="{{ route('why') }}" class="mdui-btn mdui-ripple mdui-ripple-white disabled">为什么选择</a>
                 <a href="{{ route('about_us') }}" class="mdui-btn mdui-ripple mdui-ripple-white disabled">关于我们</a>
-                <a href="{{ route('login') }}" class="mdui-btn mdui-ripple mdui-ripple-white">登录</a>
             @else
                 <a href="{{ route('main') }}" class="mdui-ripple mdui-ripple-white">Light App Engine</a>
+                <a href="{{ route('billing.index') }}" class="mdui-ripple mdui-ripple-white">剩余积分:
+                    {{ Auth::user()->balance }}</a>
                 <a target="_blank" href="https://f.lightart.top/t/knowledge-base"
                     class="mdui-btn mdui-ripple mdui-ripple-white">知识库</a>
                 <a href="{{ route('projects.index') }}" class="mdui-ripple mdui-ripple-white">项目管理</a>
                 <a href="{{ route('lxd.index') }}" class="mdui-ripple mdui-ripple-white">容器管理</a>
                 <a href="{{ route('remote_desktop.index') }}" class="mdui-ripple mdui-ripple-white">共享的 Windows</a>
                 <a target="_blank" href="https://f.lightart.top/" class="mdui-btn mdui-ripple mdui-ripple-white">社区</a>
-                <a href="{{ route('billing.index') }}" class="mdui-ripple mdui-ripple-white">剩余积分:
-                    {{ Auth::user()->balance }}</a>
                 <a onclick="event.preventDefault();document.getElementById('logout-form').submit();"
                     class="mdui-ripple mdui-ripple-white">退出登录</a>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
