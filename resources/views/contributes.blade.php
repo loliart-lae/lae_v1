997420dca84b184bdd6fcfd9f3d355fe420c8b9d
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-11 18:53:35 +0800

    Update contributes.blade.php

--- resources/views/contributes.blade.php
+++ resources/views/contributes.blade.php
@@ -48,6 +48,10 @@
                             <div class="mdui-card-primary-title">Title</div>
                             <div class="mdui-card-primary-subtitle">Subtitle</div>
                         </div>
+                        <div class="mdui-card-actions">
+                            <button class="mdui-btn mdui-ripple mdui-ripple-white">Blog</button>
+                            <button class="mdui-btn mdui-ripple mdui-ripple-white">Github</button>
+                        </div>
                     </div>
                 </div>
             </div>
