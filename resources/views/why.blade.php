0f4bc8622f2c4d7dcc5046b5f39c54c22cc3c762
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-12 16:41:17 +0800

    Update why.blade.php

--- resources/views/why.blade.php
+++ resources/views/why.blade.php
@@ -4,29 +4,31 @@
 
 @section('content')
 
-<div class="mdui-table-fluid">
-    <div class="mdui-table">
-        <thead>
-            <th></th>
-            <th>
-                Linux 容器
-            </th>
-            <th>
-                共享 Windows 远程桌面
-            </th>
-        </thead>
-        <tbody>
-            <tr>
-                <td>按分钟计费</td>
-                <td class="is-visible">支持</td>
-                <td class="is-visible">支持</td>
-            </tr>
-            <tr>
-                <td>随时修改配置</td>
-                <td class="is-visible">支持</td>
-                <td class="is-hidden">不支持</td>
-            </tr>
-        </tbody>
+<div class="mdui-typo">
+    <div class="mdui-table-fluid">
+        <div class="mdui-table">
+            <thead>
+                <th></th>
+                <th>
+                    Linux 容器
+                </th>
+                <th>
+                    共享 Windows 远程桌面
+                </th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>按分钟计费</td>
+                    <td class="is-visible">支持</td>
+                    <td class="is-visible">支持</td>
+                </tr>
+                <tr>
+                    <td>随时修改配置</td>
+                    <td class="is-visible">支持</td>
+                    <td class="is-hidden">不支持</td>
+                </tr>
+            </tbody>
+        </div>
     </div>
 </div>
 
