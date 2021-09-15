f371534fa93480168672497e605efe519bbb30c2
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-16 00:19:53 +0800

    Update contributes.blade.php

--- resources/views/contributes.blade.php
+++ resources/views/contributes.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 
-    <div class="mdui-card mdui-center mdui-container mdui-p-b-1" style="background-color: #e8e8e8">
+    <div class="mdui-card mdui-center mdui-container" style="background-color: #e8e8e8">
         <div class="mdui-container mdui-m-t-3 mdui-m-b-1">
             <div class="mdui-row">
                 <div class="mdui-col-xs-8">
@@ -23,8 +23,8 @@
                 <p>为了更方便的与用户们交流与提供更加实时的技术支持我们创建了一个 QQ 群：769779712</p>
             </div>
         </div>
-        
     </div>
+    <br />
 
     <div class="mdui-row">
         <div class="mdui-typo-display-2">非常感谢以下人员对 Light App Engine 的贡献</div><br>
