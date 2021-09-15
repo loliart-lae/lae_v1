95b71a374149a9a180ffe0c5426ef9bd8a258960
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-16 00:12:58 +0800

    Update contributes.blade.php

--- resources/views/contributes.blade.php
+++ resources/views/contributes.blade.php
@@ -16,8 +16,8 @@
                 </div>
             </div>
 
-            <div class="mdui-row">
-                <div class="mdui-typo-headline-opacity">为了更方便的与用户们交流与提供更加实时的技术支持我们创建了一个 QQ 群：769779712</div>
+            <div class="mdui-row mdui-m-t-1">
+                <p>为了更方便的与用户们交流与提供更加实时的技术支持我们创建了一个 QQ 群：769779712</p>
             </div>
         </div>
         
