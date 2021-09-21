503a4e2c9c08edce80be1b7d1c5994e191d274ae
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-21 11:36:24 +0800

    改进 避免 Safari 将数字识别成电话号码

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no" />
+    <meta name="format-detection" content="telephone=no" />
 
     <meta name="theme-color" content="#2196F3" />
 
