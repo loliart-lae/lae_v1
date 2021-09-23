784f8a5efd10c7865640d67b9daacf5970470e89
iVampireSP.com <im@ivampiresp.com>
2021-09-23 14:20:33 +0800

    修改SERVER_ID为HOST_BY

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,7 +90,7 @@
         <div class="mdui-typo mdui-m-t-5">
             <p class="mdui-typo-caption-opacity mdui-text-center">
                 <a href="{{ route('contributes') }}">Contributors</a>
-                <br />Light App Engine on {{ env('SERVER_ID', 'LoliArt') }}<br />
+                <br />Light App Engine host by {{ env('HOST_BY') }}<br />
                 Crafted with 💗 by iVampireSP.com<br />
             </p>
         </div>
