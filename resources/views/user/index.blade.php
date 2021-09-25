437f0c6427222a0119c49693a87fdb6f66aaba1f
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-25 22:39:50 +0800

    Update index.blade.php

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <div>
+        <h1 class="mdui-text-color-theme">{{ $user->name }}</h1>
         <img class="mdui-img-circle animate__bounceIn" width="25%" src="{{ config('app.gravatar_url') }}/{{ md5($user->email) }}?s=192">
         名称：{{ $user->name }} <br />
         签名：{{ $user->bio }}
