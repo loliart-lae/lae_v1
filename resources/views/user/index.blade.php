994f0e18ab3401715b5be7448cad9906621af49f
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-09-25 23:01:02 +0800

    Update index.blade.php

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,17 +4,15 @@
 
 @section('content')
     <div>
-        <h1 class="mdui-text-color-theme">{{ $user->name }}</h1>
+    <h1 class="mdui-text-color-theme">你的信息</h1>
 
         <div class="mdui-row">
-            <div class="mdui-col-xs-3">
+            <div class="mdui-col-xs-12 mdui-col-sm-5">
                 <img class="mdui-img-circle animate__bounceIn" src="{{ config('app.gravatar_url') }}/{{ md5($user->email) }}?s=192">
             </div>
 
-            <div class="mdui-col-xs-6">
-                <!--
-                <div class="mdui-typo-display-1">你的签名：<small>{{ $user->bio }}</small></div>
-                -->
+            <div class="mdui-col-xs-6 mdui-col-sm-7 mdui-text-right">
+                <div class="mdui-typo-display-1">{{ $user->name }}</div>
 
                 <form method="POST" action="{{ route('user.update', $user->id) }}">
                     @csrf
