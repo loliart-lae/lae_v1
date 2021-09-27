5e9e2aa89ead3686785cc6ef19c18f14abdd83e7
iVampireSP.com <im@ivampiresp.com>
2021-09-27 23:50:44 +0800

    改进 首页显示

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,17 +25,32 @@
             <h2 class="mdui-text-color-theme">嗯？这是什么！</h2>
             <p>往简单了说，你可以在这里充值积分。创建自己的按时计费的容器，你可以在容器中做你想做的事情，前提是不违规。在你不想使用或者没有足够的余额的时候，我们会收回它。这也意味着，你得准备好充足的积分，确保你的容器能够稳定持久的运行。
             </p>
+            <p>iVampireSP: 啊哈，这里不止有容器啦，还有“共享的 Windows”、“穿透隧道”、“Grafana”等。Light App Engine 不是我一人打造的，而是整个社区一同打造的。</p>
 
-            <h2 class="mdui-text-color-theme">我应该如何来创建容器？</h2>
+            <h2 class="mdui-text-color-theme">我应该如何来创建 Linux 容器？</h2>
             <p>让我们来走一下创建容器的流程：</p>
             <ol>
                 <li>创建/进入<abbr title="位于顶部的 “项目管理”">项目</abbr></li>
                 <li>确保<abbr title="位于顶部的 “项目管理”">项目</abbr>有足够的<abbr title="如果余额不足，需要往项目中充值">余额</abbr></li>
-                <li>在 <kbd>容器管理</kbd> 中新建容器</li>
+                <li>在 <kbd> Linux 容器</kbd> 中新建容器</li>
             </ol>
-            <p><em>创建容器必须要有项目，并且项目中有足够的余额</em></p>
+
+            <h2 class="mdui-text-color-theme">什么是 穿透隧道？</h2>
+            <p>穿透隧道 使用的是 Frp ，我们提供了配置文件生成，你无需编写一行配置文件，直接复制粘贴即可，很方便就能完成端口映射。</p>
+
+            <h2 class="mdui-text-color-theme">什么是 共享的 Windows?</h2>
+            <p>共享的 Windows 是一个使用远程桌面的功能，不具备管理员权限，但是你依旧可以拿他干一些需要的事情。</p>
+
+            <h2 class="mdui-text-color-theme">什么是 文档？</h2>
+            <p>文档 是 Light App Engine 偏向于社区化的一个功能，我们鼓励用户在这里撰写文档。<br />
+                在编写/阅读文档时，你还可以进行SSH。每当你撰写一份文档并受到别人认同时，账号积分会 +1。</p>
+
+            <h2 class="mdui-text-color-theme">什么是 Grafana?</h2>
+            <p>这是一个还测试的功能。我们为用户提供了一个 Grafana 面板。使用这个功能需要一定的技术含量。这个功能完全免费。</p>
 
 
+
+            <p><em>大部分操作都需要 创建项目(创建项目时完全免费)，并且项目中有足够的余额</em></p>
 
             <p>我们正在为 {{ App\Models\User::count() - 1 }} 个用户提供服务，并且时刻欢迎您的加入！</p>
             <p>如果你有想法想对我说，欢迎发送邮件至<a href="mailto:im@ivampiresp.com">im@ivampiresp.com</a>，我会认真阅读每一份邮件！</p>
@@ -74,7 +89,7 @@
 
             <p>到这里，你就可以使用容器啦~</p>
 
-            <h1 class="mdui-text-color-theme">什么是端口转发？我又如何访问我的容器？</h1>
+            <h1 class="mdui-text-color-theme">什么是 端口转发？我又如何访问我的容器？</h1>
 
             <p>端口转发可以将内网端口转发到公网中。不同服务器之间内网无法互相访问。同服务器中，您可以借助一个容器访问另一个容器。</p>
 
@@ -82,6 +97,21 @@
             <p>
 
             <p>转发时，请注意：外部端口必须大于 <kbd>1025</kbd></p>
+
+            <h2 class="mdui-text-color-theme">什么是 穿透隧道？</h2>
+            <p>穿透隧道 使用的是 Frp ，我们提供了配置文件生成，你无需编写一行配置文件，直接复制粘贴即可，很方便就能完成端口映射。</p>
+
+            <h2 class="mdui-text-color-theme">什么是 共享的 Windows?</h2>
+            <p>共享的 Windows 是一个使用远程桌面的功能，不具备管理员权限，但是你依旧可以拿他干一些需要的事情。</p>
+
+            <h2 class="mdui-text-color-theme">什么是 文档？</h2>
+            <p>文档 是 Light App Engine 偏向于社区化的一个功能，我们鼓励用户在这里撰写文档。<br />
+                在编写/阅读文档时，你还可以进行SSH。每当你撰写一份文档并受到别人认同时，账号积分会 +1。</p>
+
+            <h2 class="mdui-text-color-theme">什么是 Grafana?</h2>
+            <p>这是一个还测试的功能。我们为用户提供了一个 Grafana 面板。使用这个功能需要一定的技术含量。这个功能完全免费。</p>
+
+
 
             <h1 class="mdui-text-color-theme">我不可以使用 Light App Engine 的服务做什么？</h1>
 
