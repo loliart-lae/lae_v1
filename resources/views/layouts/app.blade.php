1c04f055bca04a2c1be939b86d114acbd7fdec0a
iVampireSP.com <im@ivampiresp.com>
2021-09-28 13:14:34 +0800

    修复 无法正常显示的问题

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -130,7 +130,14 @@
         </div>
     </div>
 
-    {!! editor_js() !!}
+    <script src="/vendor/editor.md/lib/marked.min.js"></script>
+    <script src="/vendor/editor.md/lib/prettify.min.js"></script>
+    {{-- <script src="/vendor/editor.md/lib/raphael.min.js"></script> --}}
+    <script src="/vendor/editor.md/lib/underscore.min.js"></script>
+    <script src="/vendor/editor.md/lib/sequence-diagram.min.js"></script>
+    <script src="/vendor/editor.md/lib/flowchart.min.js"></script>
+    <script src="/vendor/editor.md/lib/jquery.flowchart.min.js"></script>
+    <script src="/vendor/editor.md/js/editormd.min.js"></script>
     <script>
         $.ajaxSetup({
             headers: {
