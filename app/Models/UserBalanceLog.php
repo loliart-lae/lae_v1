418c18620bea9727ef6abb3560465615346788e1
iVampireSP.com <im@ivampiresp.com>
2021-10-04 16:56:28 +0800

    悲观锁

--- app/Models/UserBalanceLog.php
+++ app/Models/UserBalanceLog.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Exception;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,48 +14,60 @@ class UserBalanceLog extends Model
     use HasFactory;
     protected $table = 'user_balance_log';
 
-    public function cost($user_id, $value, $reason = null) {
-        $user = new User();
-        $user_balance = $user->where('id', $user_id)->first()->balance;
-        $current_balance = $user_balance - $value;
+    public function cost($user_id, $value, $reason = null)
+    {
+        try {
+            DB::beginTransaction();
+            $user = new User();
+            $user_balance = $user->where('id', $user_id)->lockForUpdate()->first()->balance;
+            $current_balance = $user_balance - $value;
 
-        if ($current_balance <= 0) {
-            return false;
+            if ($current_balance <= 0) {
+                return false;
+            }
+
+            $user->where('id', $user_id)->update(['balance' => $current_balance]);
+
+            $this->user_id = $user_id;
+            $this->method = 'cost';
+            $this->value = $value;
+            $this->reason = $reason;
+
+            $this->save();
+            DB::commit();
+        } catch (Exception $e) {
         }
 
-        $user->where('id', $user_id)->update(['balance' => $current_balance]);
+        return true;
+    }
 
-        $this->user_id = $user_id;
-        $this->method = 'cost';
-        $this->value = $value;
-        $this->reason = $reason;
+    public function charge($user_id, $value, $reason = null)
+    {
+        try {
+            DB::beginTransaction();
+            $user = new User();
+            $user_balance = $user->where('id', $user_id)->lockForUpdate()->first()->balance;
 
-        $this->save();
+            $current_balance = $user_balance + $value;
+
+            $user->where('id', $user_id)->update(['balance' => $current_balance]);
+
+            $this->user_id = $user_id;
+            $this->method = 'charge';
+            $this->value = $value;
+            $this->reason = $reason;
+
+            $this->save();
+            DB::commit();
+        } catch (Exception $e) {
+        }
+
 
         return true;
     }
 
-    public function charge($user_id, $value, $reason = null) {
-
-        $user = new User();
-        $user_balance = $user->where('id', $user_id)->first()->balance;
-
-        $current_balance = $user_balance + $value;
-
-        $user->where('id', $user_id)->update(['balance' => $current_balance]);
-
-        $this->user_id = $user_id;
-        $this->method = 'charge';
-        $this->value = $value;
-        $this->reason = $reason;
-
-        $this->save();
-
-        return true;
-
-    }
-
-    static public function getBalance() {
+    static public function getBalance()
+    {
         return Auth::user()->balance;
     }
 }
