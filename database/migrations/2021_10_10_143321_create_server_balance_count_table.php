e5c601eeccec66e32212cb9b3ace32bda0b59625
iVampireSP.com <im@ivampiresp.com>
2021-10-10 14:54:53 +0800

    添加 服务器流水日统计

---
+++ database/migrations/2021_10_10_143321_create_server_balance_count_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServerBalanceCountTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('server_balance_count', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('server_id')->nullable()->index();
+            $table->foreign('server_id')->references('id')->on('servers')->cascadeOnDelete();
+
+            $table->unsignedDouble('value')->default(0)->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('server_balance_count');
+    }
+}
