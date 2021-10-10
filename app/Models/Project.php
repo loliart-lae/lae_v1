a5b13c5ff223de3ac378641630b55eff9565a3d9
iVampireSP.com <im@ivampiresp.com>
2021-10-10 14:46:21 +0800

    改进 项目扣费

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 use App\Models\LxdContainer;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model
 {
@@ -18,7 +19,8 @@ class Project extends Model
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
-    public function template() {
+    public function template()
+    {
         return $this->belongsTo(LxdTemplate::class, 'template_id', 'id');
     }
 
@@ -27,7 +29,43 @@ class Project extends Model
         return $this->hasMany(LxdContainer::class, 'id', 'project_id');
     }
 
-    public function user_in_project_member() {
+    public function user_in_project_member()
+    {
         return $this->hasMany(ProjectMember::class, 'id', 'project_id');
+    }
+
+    public function cost($project_id, $value, $reason = null)
+    {
+        try {
+            DB::beginTransaction();
+            $proj_balance = self::where('id', $project_id)->lockForUpdate()->first()->balance;
+            $current_balance = $proj_balance - $value;
+
+            if ($current_balance <= 0) {
+                return false;
+            }
+
+            self::where('id', $project_id)->update(['balance' => $current_balance]);
+            DB::commit();
+        } catch (\Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function charge($project_id, $value, $reason = null)
+    {
+        try {
+            DB::beginTransaction();
+            $proj_balance = self::where('id', $project_id)->lockForUpdate()->first()->balance;
+            $current_balance = $proj_balance + $value;
+            self::where('id', $project_id)->update(['balance' => $current_balance]);
+            DB::commit();
+        } catch (\Exception $e) {
+            return false;
+        }
+
+        return true;
     }
 }
