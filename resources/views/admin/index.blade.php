71bd732d3344a32080e5584caca45745f45c2aee
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-10-11 18:31:41 +0800

    改进 修改管理员后台在小屏幕每行显示个数

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,32 +10,32 @@
         <div class="mdui-card mdui-p-a-2" style="border-radius: 8px;">
             <div class="mdui-typo-headline">数据总量</div>
 
-            <div class="mdui-col-xs-12 mdui-col-sm-2 mdui-m-t-2">
+            <div class="mdui-col-xs-6 mdui-col-sm-2 mdui-m-t-2">
                 <div class="mdui-typo-body-1-opacity">注册用户数量</div>
                 <div class="mdui-typo-display-1 mdui-m-t-1">{{ App\Models\User::count() }}</div>
             </div>
 
-            <div class="mdui-col-xs-12 mdui-col-sm-2 mdui-m-t-2">
+            <div class="mdui-col-xs-6 mdui-col-sm-2 mdui-m-t-2">
                 <div class="mdui-typo-body-1-opacity">Linux 容器 数量</div>
                 <div class="mdui-typo-display-1 mdui-m-t-1">{{ App\Models\LxdContainer::count() }}</div>
             </div>
 
-            <div class="mdui-col-xs-12 mdui-col-sm-2 mdui-m-t-2">
+            <div class="mdui-col-xs-6 mdui-col-sm-2 mdui-m-t-2">
                 <div class="mdui-typo-body-1-opacity">远程桌面 数量</div>
                 <div class="mdui-typo-display-1 mdui-m-t-1">{{ App\Models\RemoteDesktop::count() }}</div>
             </div>
 
-            <div class="mdui-col-xs-12 mdui-col-sm-2 mdui-m-t-2">
+            <div class="mdui-col-xs-6 mdui-col-sm-2 mdui-m-t-2">
                 <div class="mdui-typo-body-1-opacity">Tunnel 数量</div>
                 <div class="mdui-typo-display-1 mdui-m-t-1">{{ App\Models\Tunnel::count() }}</div>
             </div>
             
-            <div class="mdui-col-xs-12 mdui-col-sm-2 mdui-m-t-2">
+            <div class="mdui-col-xs-6 mdui-col-sm-2 mdui-m-t-2">
                 <div class="mdui-typo-body-1-opacity">FastVisit 数量</div>
                 <div class="mdui-typo-display-1 mdui-m-t-1">{{ App\Models\FastVisit::count() }}</div>
             </div>
 
-            <div class="mdui-col-xs-12 mdui-col-sm-2 mdui-m-t-2">
+            <div class="mdui-col-xs-6 mdui-col-sm-2 mdui-m-t-2">
                 <div class="mdui-typo-body-1-opacity">文档 数量</div>
                 <div class="mdui-typo-display-1 mdui-m-t-1">{{ App\Models\Document::count() }}</div>
             </div>
