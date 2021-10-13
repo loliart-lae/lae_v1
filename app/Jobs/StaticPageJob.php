7bfc112a91745e60b52aeba096abc792c2d701d9
iVampireSP.com <im@ivampiresp.com>
2021-10-13 21:15:32 +0800

    改进 错误处理

--- app/Jobs/StaticPageJob.php
+++ app/Jobs/StaticPageJob.php
@@ -48,13 +48,15 @@ class StaticPageJob implements ShouldQueue
                         'token' => $this->config['token']
                     ]);
 
+                    if ($result['status'] != 1) {
+                        throw new Exception('error read response.');
+                    }
+
                     $staticPage->where('id', $this->config['inst_id'])->update([
                         'status' => 'active',
                     ]);
 
                     Message::send('成功新建了 静态托管。', $this->config['user']);
-
-
                 } catch (Exception $e) {
                     $staticPage->where('id', $this->config['inst_id'])->delete();
                     Message::send('此时无法新建 静态托管。', $this->config['user']);
@@ -69,11 +71,15 @@ class StaticPageJob implements ShouldQueue
                 break;
 
             case 'delete':
-                Http::retry(5, 100)->get("http://{$this->config['address']}/site/delete", [
+
+
+                $result = Http::retry(5, 100)->get("http://{$this->config['address']}/site/delete", [
                     'id' => $this->config['inst_id'],
                     'token' => $this->config['token']
                 ]);
-
+                if ($result['status'] != 1) {
+                    throw new Exception('error read response.');
+                }
                 break;
 
             case 'passwd':
