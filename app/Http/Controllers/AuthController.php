e286ed6eefa35a2c8ac9443fa1f3a95b5298c4cf
iVampireSP.com <im@ivampiresp.com>
2021-10-14 22:42:08 +0800

    更新 用户登录后展示的内容

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -52,7 +52,7 @@ class AuthController extends Controller
         ]);
 
 
-        return redirect()->route('index');
+        return redirect()->route('main');
     }
 
     public function logout()
