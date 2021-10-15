0b43bd66246cd14b131e29b5e9e11af9e26f4897
冰砚炽 <Bing_Yanchi@outlook.com>
2021-10-15 11:02:30 +0800

    回滚操作

--- resources/views/components/choose-project-form.blade.php
+++ resources/views/components/choose-project-form.blade.php
@@ -1,37 +1,35 @@
-<div class="mdui-container">
-    <div class="mdui-row mdui-p-y-1">
-        <span class="mdui-typo-headline">选择项目</span>
-    </div>
+<div class="mdui-row mdui-p-y-1">
+    <span class="mdui-typo-headline">选择项目</span>
+</div>
 
-    <div class="mdui-table-fluid">
-        <table class="mdui-table mdui-table-hoverable">
-            <thead>
+<div class="mdui-table-fluid">
+    <table class="mdui-table mdui-table-hoverable">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>名称</th>
+                <th>项目积分</th>
+                <th>选择</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php($i = 1)
+            @foreach ($projects as $project)
                 <tr>
-                    <th>ID</th>
-                    <th>名称</th>
-                    <th>项目积分</th>
-                    <th>选择</th>
+                    <td nowrap="nowrap">{{ $i++ }}</td>
+                    <td nowrap="nowrap">{{ $project->project->name }}</td>
+                    <td nowrap="nowrap">{{ $project->project->balance }}</td>
+
+                    <td>
+                        <label class="mdui-radio">
+                            <input type="radio" value="{{ $project->project->id }}" name="project_id"
+                                @if ($i == 2) checked @endif required />
+                            <i class="mdui-radio-icon"></i>
+
+                        </label>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                @php($i = 1)
-                @foreach ($projects as $project)
-                    <tr>
-                        <td nowrap="nowrap">{{ $i++ }}</td>
-                        <td nowrap="nowrap">{{ $project->project->name }}</td>
-                        <td nowrap="nowrap">{{ $project->project->balance }}</td>
-
-                        <td>
-                            <label class="mdui-radio">
-                                <input type="radio" value="{{ $project->project->id }}" name="project_id"
-                                    @if ($i == 2) checked @endif required />
-                                <i class="mdui-radio-icon"></i>
-
-                            </label>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+            @endforeach
+        </tbody>
+    </table>
 </div>
