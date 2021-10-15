d4d85aaa95b1c15290c7b7ac924919855404a407
iVampireSP.com <im@ivampiresp.com>
2021-10-15 10:12:37 +0800

    增加 修改密码提示

--- resources/views/remote_desktop/edit.blade.php
+++ resources/views/remote_desktop/edit.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
-@section('title', '修改密码')
+@section('title', '修改密码并注销')
 
 @section('content')
-    <div class="mdui-typo-display-2">修改密码</div>
+    <div class="mdui-typo-display-2">修改密码并注销</div>
 
     <br />
     <form method="post" action="{{ route('remote_desktop.update', $id) }}">
@@ -14,7 +14,8 @@
             <label class="mdui-textfield-label">正在修改“{{ $rdp->username }}”的密码</label>
             <input class="mdui-textfield-input" type="password" name="password" />
         </div>
+        <p>新密码可以和原密码相同，但是不能为空。修改后会注销账号。</p>
         <br />
-        <button type="submit" class="mdui-btn mdui-color-theme-accent mdui-ripple">修改</button>
+        <button type="submit" class="mdui-btn mdui-color-theme-accent mdui-ripple">修改并注销</button>
     </form>
 @endsection
