b626acaf1abfb664f0bec31be99f9e6e8049606f
iVampireSP.com <im@ivampiresp.com>
2021-10-16 17:32:42 +0800

    改进 空内容的显示

--- resources/views/remote_desktop/index.blade.php
+++ resources/views/remote_desktop/index.blade.php
@@ -54,7 +54,7 @@
                         <td nowrap="nowrap">{{ $remote_desktop->server->cpu }} Core</td>
                         <td nowrap="nowrap">{{ $remote_desktop->server->mem }}M</td>
                         <td nowrap="nowrap">{{ $remote_desktop->server->network_limit }} Mbps</td>
-                        @php($status = json_decode(Cache::get('windows_server_status_' . $remote_desktop->server->id, ['cpu' => 'null', 'mem' => 'null'])))
+                        @php($status = json_decode(Cache::get('windows_server_status_' . $remote_desktop->server->id, json_encode(['cpu' => 'null', 'mem' => 'null']))))
                         <td nowrap="nowrap">{{ $status->cpu }}%</td>
                         <td nowrap="nowrap">{{ $status->mem }}%</td>
                         <td nowrap="nowrap">{{ $remote_desktop->server->name }}</td>
