8aab617c4dcb597f4548da2f321f33a9aa21f772
iVampireSP.com <im@ivampiresp.com>
2021-10-18 09:00:21 +0800

    改进 动画显示

--- resources/views/include/_loading.blade.php
+++ resources/views/include/_loading.blade.php
@@ -1,5 +1,5 @@
-<div class="mdui-container mdui-m-t-5 mdui-m-b-5 mdui-text-center" id="load-spinner" style="opacity: 0;display:none">
+<div class="mdui-container mdui-m-b-5 mdui-text-center" id="load-spinner"
+    style="transition: all .3s ease-in-out;opacity: 1;display:block;position:fixed;width:100%;top:0">
     <div class="mdui-spinner"></div>
     <div id="load-spinner-text" class="mdui-m-t-1 mdui-typo-caption-opacity mdui-text-center" style="opacity: 0"></div>
-
 </div>
