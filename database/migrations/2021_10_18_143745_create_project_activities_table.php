6baaa80ff35f6a91779f28a5a1421002af9542ac
iVampireSP.com <im@ivampiresp.com>
2021-10-18 15:42:16 +0800

    增加 项目事件记录迁移

---
+++ database/migrations/2021_10_18_143745_create_project_activities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('msg')->index();
+
+            $table->unsignedBigInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('project_id')->index();
+            $table->foreign('project_id')->references('id')->on('projects');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_activities');
+    }
+}
