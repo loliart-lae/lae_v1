76d48700b63449bf9dc7953558b6a8fdbff7ba1b
冰砚炽 <Bing_Yanchi@outlook.com>
2021-10-18 11:57:16 +0800

    Update global.blade.php

--- resources/views/global.blade.php
+++ resources/views/global.blade.php
@@ -5,9 +5,7 @@
 @section('content')
     <div class="mdui-typo">
         <span class="mdui-typo-headline">嗨, {{ Auth::user()->name }}。</span>
-        <br />
         <span class="mdui-typo-headline-opacity hitokoto_text"></span>
-        <br /><br />
 
         <form method="POST" action="{{ route('status.store') }}">
             @csrf
