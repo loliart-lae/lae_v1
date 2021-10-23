3ea5640dc554b1f39e5203d7fa6cb32591d692b9
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-10-23 20:44:37 +0800

    改进 新增图标

--- resources/views/components/main-menu.blade.php
+++ resources/views/components/main-menu.blade.php
@@ -2,25 +2,23 @@
     <div class="mdui-list" id="main-list">
         @guest
             <a class="mdui-list-item mdui-ripple umami--click--main-link" href="{{ route('index') }}">
-                <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
                 <div class="mdui-list-item-content">{{ config('app.name') }}</div>
             </a>
             <a class="mdui-list-item mdui-rippl umami--click--gust-login" href="{{ route('login') }}">
-                <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
+                <i class="mdui-list-item-icon mdui-icon material-icons">assignment_ind</i>
                 <div class="mdui-list-item-content">登录</div>
             </a>
             <a class="mdui-list-item mdui-ripple umami--click--why-begin" href="{{ route('why_begin') }}">
-                <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
+                <i class="mdui-list-item-icon mdui-icon material-icons">directions_run</i>
                 <div class="mdui-list-item-content">我们的初心</div>
             </a>
         @else
             <a class="mdui-list-item mdui-ripple umami--click--main-link" href="{{ route('main') }}">
-                <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
                 <div class="mdui-list-item-content">{{ config('app.name') }}</div>
             </a>
 
             <a class="mdui-list-item mdui-ripple umami--click--user-index" href="{{ route('user.index') }}">
-                <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
+                <i class="mdui-list-item-icon mdui-icon material-icons">account_circle</i>
                 <div class="mdui-list-item-content"><small>
                         {{ Auth::user()->name }} / <span id="userBalance"
                             style="display: contents;">{{ Auth::user()->balance }}</span></small></div>
@@ -58,23 +56,23 @@
             </a>
 
             <a class="mdui-list-item mdui-ripple umami--click--staticPage" href="{{ route('staticPage.index') }}">
-                <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
+                <i class="mdui-list-item-icon mdui-icon material-icons">web</i>
                 <div class="mdui-list-item-content">静态站点</div>
             </a>
 
             <a class="mdui-list-item mdui-ripple umami--click--document" href="{{ route('documents.index') }}">
-                <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
+                <i class="mdui-list-item-icon mdui-icon material-icons">art_track</i>
                 <div class="mdui-list-item-content">文档中心</div>
             </a>
 
             <a class="mdui-list-item mdui-ripple umami--click--forum" target="_blank" href="https://f.lightart.top">
-                <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
+                <i class="mdui-list-item-icon mdui-icon material-icons">link</i>
                 <div class="mdui-list-item-content">社区论坛</div>
             </a>
 
             <a onclick="event.preventDefault();document.getElementById('logout-form').submit();"
                 class="mdui-list-item mdui-ripple umami--click--logout" target="_blank" href="https://f.lightart.top">
-                <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
+                <i class="mdui-list-item-icon mdui-icon material-icons">exit_to_app</i>
                 <div class="mdui-list-item-content">退出登录</div>
             </a>
 
