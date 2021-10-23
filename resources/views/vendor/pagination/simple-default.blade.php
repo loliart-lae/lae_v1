0d5fd037c409cf0632521ae7083f14229cc16db1
iVampireSP.com <im@ivampiresp.com>
2021-10-23 16:22:26 +0800

    改进 分页样式

--- resources/views/vendor/pagination/simple-default.blade.php
+++ resources/views/vendor/pagination/simple-default.blade.php
@@ -1,16 +1,19 @@
-@if ($paginator->hasPages())
-    @if ($paginator->onFirstPage())
-        <button disabled class="mdui-float-left mdui-btn mdui-color-theme-accent mdui-ripple">上一页</button>
-    @else
-        <a href="{{ $paginator->previousPageUrl() }}"
-            class="mdui-float-left mdui-btn mdui-color-theme-accent mdui-ripple">上一页</a>
+<div class="mdui-m-t-2">
+    @if ($paginator->hasPages())
+        @if ($paginator->onFirstPage())
+            <button disabled class="mdui-float-left mdui-btn mdui-color-theme-accent mdui-ripple">上一页</button>
+        @else
+            <a href="{{ $paginator->previousPageUrl() }}"
+                class="mdui-float-left mdui-btn mdui-color-theme-accent mdui-ripple">上一页</a>
+        @endif
+
+        @if ($paginator->hasMorePages())
+            <a href="{{ $paginator->nextPageUrl() }}"
+                class="mdui-float-right mdui-btn mdui-color-theme-accent mdui-ripple">下一页</a>
+        @else
+            <button disabled class="mdui-float-right mdui-btn mdui-color-theme-accent mdui-ripple">下一页</button>
+        @endif
+
     @endif
 
-    @if ($paginator->hasMorePages())
-        <a href="{{ $paginator->nextPageUrl() }}"
-            class="mdui-float-right mdui-btn mdui-color-theme-accent mdui-ripple">下一页</a>
-    @else
-        <button disabled class="mdui-float-right mdui-btn mdui-color-theme-accent mdui-ripple">下一页</button>
-    @endif
-
-@endif
+</div>
