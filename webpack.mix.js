18f1f198071e44ba28e9ac11da389ec1910b9ff7
iVampireSP.com <im@ivampiresp.com>
2021-10-23 19:26:52 +0800

    测试开发环境

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js("resources/js/app.js", "public/js")
-    .postCss("resources/css/app.css", "public/css", []).version();
-
+mix.js("resources/js/app.js", "public/js").version()
+mix.postCss("resources/css/app.css", "public/css").version();
 mix.js("resources/js/pjax.js", "public/js").version()
+mix.disableNotifications();
