e7e54c0ace4f1df305ca1cbc1f38b65f10bf2caa
iVampireSP.com <im@ivampiresp.com>
2021-10-25 11:28:20 +0800

    增加  超时时间

--- app/Jobs/ServerStatusJob.php
+++ app/Jobs/ServerStatusJob.php
@@ -40,14 +40,13 @@ class ServerStatusJob implements ShouldQueue
         // 获取 Windows 服务器 资源占用
         foreach ($windows_servers as $windows_server) {
             try {
-                $result = Http::get("http://{$windows_server->address}/status", [
+                $result = Http::timeout(5)->get("http://{$windows_server->address}/status", [
                     'token' => $windows_server->token
                 ]);
             } catch (Exception $e) {
                 $result['cpu'] = 'unknown';
                 $result['mem'] = 'unknown';
             }
-
 
             Cache::put('windows_server_status_' . $windows_server->id, json_encode([
                 'cpu' => $result['cpu'],
