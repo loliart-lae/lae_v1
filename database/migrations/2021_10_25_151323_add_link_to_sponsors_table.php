e7d0f97d344b2158b85fd4f784e961f81d380861
iVampireSP.com <im@ivampiresp.com>
2021-10-25 15:14:08 +0800

    添加 默认值

---
+++ database/migrations/2021_10_25_151323_add_link_to_sponsors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLinkToSponsorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sponsors', function (Blueprint $table) {
+            $table->string('link')->default('#')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sponsors', function (Blueprint $table) {
+            //
+        });
+    }
+}
