4f1fd49f6967e9dffbfc5624264cc51886ffab1e
iVampireSP.com <im@ivampiresp.com>
2021-10-27 08:48:16 +0800

    改进 页面优化

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,11 +23,11 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Spatie\Pjax\Middleware\FilterIfPjax::class,
         // \RenatoMarinho\LaravelPageSpeed\Middleware\InlineCss::class,
-        \RenatoMarinho\LaravelPageSpeed\Middleware\ElideAttributes::class,
+        // \RenatoMarinho\LaravelPageSpeed\Middleware\ElideAttributes::class,
         \RenatoMarinho\LaravelPageSpeed\Middleware\InsertDNSPrefetch::class,
         \RenatoMarinho\LaravelPageSpeed\Middleware\RemoveComments::class,
         // \RenatoMarinho\LaravelPageSpeed\Middleware\TrimUrls::class,
-        \RenatoMarinho\LaravelPageSpeed\Middleware\RemoveQuotes::class,
+        // \RenatoMarinho\LaravelPageSpeed\Middleware\RemoveQuotes::class,
         // \RenatoMarinho\LaravelPageSpeed\Middleware\CollapseWhitespace::class,
         // \RenatoMarinho\LaravelPageSpeed\Middleware\DeferJavascript::class,
     ];
