1e18a4defa62b3376aa50d7f7da8e342610fac7b
iVampireSP.com <im@ivampiresp.com>
2021-10-29 10:40:24 +0800

    修复 瀑布流显示问题

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -16,43 +16,40 @@
         @endif
         待处理的邀请
     </a>
-    <div class="mdui-row mdui-m-t-2">
+    <div id="masonry" class="mdui-row">
+        @foreach ($projects as $project)
+            <div class="mdui-col-sm-4 mdui-col-xs-12 mdui-m-t-1 poll">
+                <div>
+                    <h3 id="proj-{{ $project->project->id }}" class="scroll_listen mdui-text-color-theme">
+                        {{ $project->project->name }}</h3>
+                    <div class="mdui-card mdui-hoverable">
+                        <div class="mdui-card-primary">
+                            <div class="mdui-card-primary-title">{{ $project->project->name }}</div>
+                            <div class="mdui-card-primary-subtitle">{{ $project->project->description }}</div>
+                            <div class="mdui-card-primary-subtitle">项目积分：{{ $project->project->balance }}
+                                @if ($project->project->balance < 100) !积分过少! @endif</div>
+                            <div class="mdui-card-primary-subtitle">创建时间：{{ $project->project->created_at }}</div>
+                        </div>
+                        <div class="mdui-card-actions">
+                            <a href="{{ route('projects.show', $project->project->id) }}"
+                                class="mdui-btn mdui-ripple umami--click--project-manage">管理</a>
 
-        <div id="masonry" class="mdui-row">
-            @foreach ($projects as $project)
-                <div class="mdui-col-sm-6 pull">
-                    <div>
-                        <h3 id="proj-{{ $project->project->id }}" class="scroll_listen mdui-text-color-theme">
-                            {{ $project->project->name }}</h3>
-                        <div class="mdui-card mdui-hoverable">
-                            <div class="mdui-card-primary">
-                                <div class="mdui-card-primary-title">{{ $project->project->name }}</div>
-                                <div class="mdui-card-primary-subtitle">{{ $project->project->description }}</div>
-                                <div class="mdui-card-primary-subtitle">项目积分：{{ $project->project->balance }}
-                                    @if ($project->project->balance < 100) !积分过少! @endif</div>
-                                <div class="mdui-card-primary-subtitle">创建时间：{{ $project->project->created_at }}</div>
-                            </div>
-                            <div class="mdui-card-actions">
-                                <a href="{{ route('projects.show', $project->project->id) }}"
-                                    class="mdui-btn mdui-ripple umami--click--project-manage">管理</a>
-
-                                @if ($project->project->user_id != Auth::id())
-                                    <form style="display: inline;" method="POST"
-                                        action="{{ route('projects.leave', $project->project->id) }}">
-                                        @csrf
-                                        <button onclick="if(!confirm('离开后，将不会退回任何资金，也无法撤销该操作。')) return false"
-                                            class="mdui-btn mdui-ripple umami--click--project-leave">离开</button>
-                                    </form>
-                                @else
-                                    <a href="{{ route('projects.edit', $project->project->id) }}"
-                                        class="mdui-btn mdui-ripple umami--click--project-edit">修改</a>
-                                @endif
-                            </div>
+                            @if ($project->project->user_id != Auth::id())
+                                <form style="display: inline;" method="POST"
+                                    action="{{ route('projects.leave', $project->project->id) }}">
+                                    @csrf
+                                    <button onclick="if(!confirm('离开后，将不会退回任何资金，也无法撤销该操作。')) return false"
+                                        class="mdui-btn mdui-ripple umami--click--project-leave">离开</button>
+                                </form>
+                            @else
+                                <a href="{{ route('projects.edit', $project->project->id) }}"
+                                    class="mdui-btn mdui-ripple umami--click--project-edit">修改</a>
+                            @endif
                         </div>
                     </div>
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endforeach
     </div>
 
     <script>
