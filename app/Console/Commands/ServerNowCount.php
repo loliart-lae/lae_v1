64e4621f517fc3af0f836cacfc2e397c1f589389
iVampireSP.com <im@ivampiresp.com>
2021-10-29 10:48:45 +0800

    修改 内存限制

--- app/Console/Commands/ServerNowCount.php
+++ app/Console/Commands/ServerNowCount.php
@@ -39,6 +39,7 @@ class ServerNowCount extends Command
      */
     public function handle()
     {
+        ini_set('memory_limit', '1024M');
         DB::connection()->disableQueryLog();
 
         $this->info('正在统计数据，可能会比较慢。');
