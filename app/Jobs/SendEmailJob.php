4b9b85a8acbf8f6ef9733b1223f19d3de93e3ed2
iVampireSP.com <im@ivampiresp.com>
2021-10-31 18:26:21 +0800

    将 env 替换为 config

--- app/Jobs/SendEmailJob.php
+++ app/Jobs/SendEmailJob.php
@@ -35,11 +35,11 @@ class SendEmailJob implements ShouldQueue
     {
         Mail::raw($this->content, function ($message) {
             // 发件人（你自己的邮箱和名称）
-            $message->from(env('MAIL_USERNAME'), env('APP_NAME'));
+            $message->from(config('mail.from.address'), config('mail.from.name'));
             // 收件人的邮箱地址
             $message->to($this->to);
             // 邮件主题
-            $message->subject(env('APP_NAME') . ' 队列通知');
+            $message->subject(config('app.name') . ' 队列通知');
         });
     }
 }
