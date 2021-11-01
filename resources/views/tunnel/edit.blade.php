5339b13b0bc56cceab705c02fbb5beba87680e4b
iVampireSP.com <im@ivampiresp.com>
2021-11-01 17:02:18 +0800

    移除 浮动标签

--- resources/views/tunnel/edit.blade.php
+++ resources/views/tunnel/edit.blade.php
@@ -12,7 +12,7 @@
 
         <span class="mdui-typo-headline">内网地址</span>
         <p>被映射主机的地址，比如 127.0.0.1:80</p>
-        <div class="mdui-textfield mdui-textfield-floating-label">
+        <div class="mdui-textfield">
             <label class="mdui-textfield-label">内网地址</label>
             <input class="mdui-textfield-input" type="text" name="local_address" value="{{ $tunnel->local_address }}"
                 required />
@@ -21,10 +21,22 @@
 
         <span class="mdui-typo-headline">隧道的名称</span>
         <p>只允许字母、数字，短破折号（-）和下划线（_）,至少 3 位，最多 15 位。该名称用于标识。</p>
-        <div class="mdui-textfield mdui-textfield-floating-label">
+        <div class="mdui-textfield">
             <label class="mdui-textfield-label">隧道名称</label>
             <input class="mdui-textfield-input" type="text" name="name" value="{{ $tunnel->name }}" required />
         </div>
+
+        <br />
+        <div class="mdui-row-md-4 mdui-m-b-2">
+            <div class="mdui-col">
+                <label class="mdui-checkbox" mdui-tooltip="{content: '如果你的隧道信息已被泄漏，那么勾选这个可能会很有用。当你勾选并提交后，使用该隧道的客户端将会被强制下线，你也必须申请新的隧道配置文件。', position: 'right'}">
+                    <input type="checkbox" name="reset_token" value="1" />
+                    <i class="mdui-checkbox-icon"></i>
+                    重置隧道认证
+                </label>
+            </div>
+        </div>
+
         <button type="submit" class="mdui-btn mdui-color-theme-accent mdui-ripple">修改</button>
     </form>
 @endsection
