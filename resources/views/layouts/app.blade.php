807f8607efe783faeef70d0f33d7ff2b7433b17f
iVampireSP.com <im@ivampiresp.com>
2021-11-02 20:31:19 +0800

    去除 主题颜色

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no" />
     <meta name="format-detection" content="telephone=no" />
 
-    <meta name="theme-color" content="#2196F3" />
+    {{-- <meta name="theme-color" content="#2196F3" /> --}}
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{!! csrf_token() !!}" />
