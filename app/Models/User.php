d434a9faa68b04841e5875f06c06e73d94024634
iVampireSP.com <im@ivampiresp.com>
2021-11-09 16:09:47 +0800

    优化 代码

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,12 +68,11 @@ class User extends Authenticatable
         }
         $ids[] = Auth::id();
 
-        $result =  UserStatus::whereIn('user_id', $ids)->with(['user' => function ($query) {
+        $result = UserStatus::whereIn('user_id', $ids)->with(['user' => function ($query) {
             $query->select(['id', 'name', 'email', 'bio', 'website']);
         }, 'like' => function ($query) {
             $query->where('user_id', Auth::id());
         }])->orderBy('created_at', 'desc');
-
 
         return $result;
     }
