e799346bd4e2732c01e44f56bf2fc8b820a0d9fc
iVampireSP.com <im@ivampiresp.com>
2021-11-11 16:03:14 +0800

    客户机的基本设定

--- app/Http/Controllers/TransferBridgeGuestController.php
+++ app/Http/Controllers/TransferBridgeGuestController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\TransferBridge;
+use App\Models\TransferBridgeGroup;
+use App\Models\TransferBridgeGuest;
+use Illuminate\Support\Facades\Auth;
 
 class TransferBridgeGuestController extends Controller
 {
@@ -21,9 +25,18 @@ class TransferBridgeGuestController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $bridge = TransferBridge::with(['project', 'groups'])->whereHas('member', function ($query) {
+            $query->where('user_id', Auth::id());
+        })->orderBy('project_id')->firstOrFail();
+
+
+        if (!ProjectMembersController::userInProject($bridge->project_id)) {
+            return redirect()->back()->with('status', '你不在项目中。');
+        }
+
+        return view('bridge.guest.create',  compact('bridge'));
     }
 
     /**
@@ -32,9 +45,32 @@ class TransferBridgeGuestController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, TransferBridgeGuest $transferBridgeGuest)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|max:10',
+            'unique_id' => 'required|max:10',
+            'group_id' => 'required|integer',
+        ]);
+        // dd($request->group_id);
+        $group = TransferBridgeGroup::where('id', $request->group_id)->with('bridge')->firstOrFail();
+
+        // 检查 用户是否在项目中
+        if (!ProjectMembersController::userInProject($group->bridge->project_id)) {
+            return redirect()->back()->with('status', '你不在项目中。');
+        }
+
+        // 检查是否存在
+        if ($transferBridgeGuest->where('unique_id', $request->unique_id)->exists()) {
+            return redirect()->back()->with('status', '唯一 ID 已存在。');
+        }
+
+        $transferBridgeGuest->name = $request->name;
+        $transferBridgeGuest->unique_id = $request->unique_id;
+        $transferBridgeGuest->transfer_bridge_group_id = $request->group_id;
+        $transferBridgeGuest->save();
+
+        return redirect()->route('bridge.groups.index', $group->transfer_bridge_id)->with('status', '创建成功。');
     }
 
     /**
