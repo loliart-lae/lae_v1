7cb82a6540ef28d79b53bc65c158a816e61e368f
iVampireSP.com <im@ivampiresp.com>
2021-11-12 19:37:40 +0800

    增加 鉴权密码

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
     'streaming_control_path' => env('STREAMING_CONTROL_PATH', '/stream_control'),
     'streaming_control_auth' => env('STREAMING_CONTROL_AUTH', 'root'),
     'streaming_control_password' => env('STREAMING_CONTROL_PASSWORD', 123456),
+    'streaming_validate_password' => env('STREAMING_VALIDATE_PASSWORD', 1234567890),
 
 
     /*
