9c5257c53d71aacf102a031f71ba36b2173e48be
iVampireSP.com <im@ivampiresp.com>
2021-11-12 11:36:29 +0800

    改进 登录处理

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use hanbz\PassportClient\Facades\PassportClient;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use hanbz\PassportClient\Facades\PassportClient;
+use hanbz\PassportClient\Two\InvalidStateException;
 
 class AuthController extends Controller
 {
@@ -16,43 +17,46 @@ class AuthController extends Controller
         }
         return PassportClient::driver('passport')->redirect();
     }
+
     public function OAuthCallback()
     {
-        $oauth_user = PassportClient::driver('passport')->user();
-        // dd($oauth_user);
-        $user_sql = User::where('email', $oauth_user->getEmail());
-        $user = $user_sql->first();
+        try {
+            $oauth_user = PassportClient::driver('passport')->user();
+            // dd($oauth_user);
+            $user_sql = User::where('email', $oauth_user->getEmail());
+            $user = $user_sql->first();
 
-        if (is_null($user)) {
-            $name = $oauth_user->getName();
-            $email = $oauth_user->getEmail();
-            $password = Str::random(8);
-            $email_verified_at = $oauth_user->user['email_verified_at'];
-            $user = User::create(compact('name', 'email', 'password', 'email_verified_at'));
-            $user_id = User::where('email', $oauth_user->getEmail())->firstOrFail()->id;
-            ProjectController::create_project($user_id, $oauth_user->getName(), '默认项目');
-            $to = User::find(1);
-            $self = User::find($user_id);
-            $self->follow($to);
-        } else {
-            if ($user->name != $oauth_user->getName()) {
-                User::where('email', $oauth_user->getEmail())->update([
-                    'name' => $oauth_user->getName()
-                ]);
+            if (is_null($user)) {
+                $name = $oauth_user->getName();
+                $email = $oauth_user->getEmail();
+                $password = Str::random(8);
+                $email_verified_at = $oauth_user->user['email_verified_at'];
+                $user = User::create(compact('name', 'email', 'password', 'email_verified_at'));
+                $user_id = User::where('email', $oauth_user->getEmail())->firstOrFail()->id;
+                ProjectController::create_project($user_id, $oauth_user->getName(), '默认项目');
+                $to = User::find(1);
+                $self = User::find($user_id);
+                $self->follow($to);
+            } else {
+                if ($user->name != $oauth_user->getName()) {
+                    User::where('email', $oauth_user->getEmail())->update([
+                        'name' => $oauth_user->getName()
+                    ]);
+                }
+
+                Auth::login($user);
             }
 
-            Auth::login($user);
+            // 更新最后登录时间
+            $user_sql->update([
+                'last_login_at' => now()
+            ]);
 
+            return redirect()->route('index');
+        } catch (InvalidStateException $e) {
+            unset($e);
+            return redirect()->route('login');
         }
-
-
-        // 更新最后登录时间
-        $user_sql->update([
-            'last_login_at' => now()
-        ]);
-
-
-        return redirect()->route('index');
     }
 
     public function logout()
