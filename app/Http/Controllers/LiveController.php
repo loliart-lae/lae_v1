8ec1344cb478ed4fe4617a6ebb440c3692962c92
iVampireSP.com <im@ivampiresp.com>
2021-11-12 19:40:34 +0800

    增加 流式媒体

---
+++ app/Http/Controllers/LiveController.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use LVR\Colour\Hex;
+use Illuminate\Http\Request;
+use App\Models\LiveTimePeriod;
+use Illuminate\Support\Carbon;
+use Ramsey\Uuid\Rfc4122\UuidV4;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+
+class LiveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(LiveTimePeriod $liveTimePeriod)
+    {
+        $lives = $liveTimePeriod->whereBetween('created_at', [Carbon::today()->toDateTimeString(), Carbon::tomorrow()->toDateTimeString()])->with('user')->get();
+        return view('live.index', compact('lives'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('live.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, LiveTimePeriod $liveTimePeriod)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:App\Models\LiveTimePeriod,name',
+            'start_at' => 'required',
+            'end_at' => 'required',
+        ]);
+        // dd($request->end_at);
+        // dd(Carbon::parse($request->end_at)->toTimeString());
+        if (!Carbon::parse($request->start_at)->isToday() && !Carbon::parse($request->end_at)->isToday()) {
+            return redirect()->back()->with('status', '不在时间内。');
+        }
+
+        // 检测时间差
+        $minutes = Carbon::parse($request->start_at)->diffInMinutes(Carbon::parse($request->end_at), false);
+        if (!$minutes || $minutes > 90) {
+            return redirect()->back()->with('status', '单个节目时长不能大于 90 分钟。');
+        }
+
+        // 检测当前区间是否被占用
+        $liveTimePeriod_where = $liveTimePeriod->where('end_at', '>', Carbon::parse($request->end_at)->toTimeString());
+        // dd($liveTimePeriod_where->get());
+        if ($liveTimePeriod_where->exists()) {
+            return redirect()->back()->with('status', '这个时间段已被安排了。');
+        }
+
+        $liveTimePeriod->name = $request->name;
+        $liveTimePeriod->token = UuidV4::uuid4()->toString();
+        $liveTimePeriod->start_at = $request->start_at;
+        $liveTimePeriod->end_at = $request->end_at;
+        $liveTimePeriod->user_id = Auth::id();
+        $liveTimePeriod->save();
+
+        return redirect()->back()->with('status', '时间段安排成功。');
+
+        // dd(Carbon::parse($request->start_at)->isToday());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $liveTimePeriod = new LiveTimePeriod();
+        $live = $liveTimePeriod->where('id', $id)->firstOrFail();
+        if ($live->user_id == Auth::id()) {
+            return view('live.edit', compact('live'));
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $liveTimePeriod = new LiveTimePeriod();
+        $live = $liveTimePeriod->where('id', $id)->firstOrFail();
+        if ($live->user_id == Auth::id()) {
+            $liveTimePeriod->where('id', $id)->update([
+                'name' => $request->name,
+            ]);
+            return redirect()->back()->with('status', '已修改安排。');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $liveTimePeriod = new LiveTimePeriod();
+        $liveTimePeriod = $liveTimePeriod->where('id', $id)->firstOrFail();
+        if ($liveTimePeriod->user_id == Auth::id()) {
+            $liveTimePeriod->where('id', $id)->delete();
+            return redirect()->route('live.index')->with('status', '删除成功。');
+        }
+    }
+
+    public function auth(Request $request)
+    {
+        if ($request->route('key') != config('app.streaming_application')) {
+            abort(403, 'application not found');
+        }
+
+        $liveTimePeriod = new LiveTimePeriod();
+        $liveTimePeriod = $liveTimePeriod->where('token', $request->token)->firstOrFail();
+
+        if ($request->app != config('app.streaming_application')) {
+            abort(403, 'application not found');
+        }
+
+        if ($request->app != config('app.streaming_application')) {
+            abort(403, 'application not found');
+        }
+        // Log::debug($request);
+    }
+}
