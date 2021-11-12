10f0b00930be9a83ab449df510982a1ddeba51fc
iVampireSP.com <im@ivampiresp.com>
2021-11-12 22:26:41 +0800

    移除 PREFIX 字段

--- config/app.php
+++ config/app.php
@@ -157,7 +157,6 @@ return [
     'streaming_proto' => env('STREAMING_PROTO', 'rtmp'),
     'streaming_port' => env('STREAMING_PORT', 1935),
     'streaming_address' => env('STREAMING_ADDRESS', config('app.domain')),
-    'streaming_prefix' => env('STREAMING_PREFIX', 'streaming'),
     'streaming_application' => env('STREAMING_APPLICATION', 'hls'),
     'streaming_control_path' => env('STREAMING_CONTROL_PATH', '/stream_control'),
     'streaming_control_auth' => env('STREAMING_CONTROL_AUTH', 'root'),
