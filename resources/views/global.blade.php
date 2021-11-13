0dd8e28524d29466e09c74e8d73ebcf521cc0c28
iVampireSP.com <im@ivampiresp.com>
2021-11-13 23:23:46 +0800

    全部改为 dash

--- resources/views/global.blade.php
+++ resources/views/global.blade.php
@@ -3,59 +3,48 @@
 @section('title', '全站时光长河')
 
 @section('content')
-    @auth
-        @php($live = \App\Models\LiveTimePeriod::where('status', 1)->first())
-        @if (!is_null($live))
-            <div id="streaming_div" class="mdui-typo">
-                <div class="mdui-typo-headline"><a href="{{ route('streaming') }}">{{ $live->name }}</a></div>
+@auth
+@php($live = \App\Models\LiveTimePeriod::where('status', 1)->first())
+@if (!is_null($live))
+<div id="streaming_div" class="mdui-typo">
+    <div class="mdui-typo-headline"><a href="{{ route('streaming') }}">{{ $live->name }}</a></div>
 
-                <video id="streaming" style="width:100%;border-radius:5px;margin-top:10px" controls muted autoplay playsinline></video>
-            </div>
+    <video id="streaming" style="width:100%;border-radius:5px;margin-top:10px" controls muted autoplay playsinline>
+        <source src="{{ config('app.streaming_play_url') }}" />
+    </video>
+</div>
 
-            <script>
-                var video_streaming = document.getElementById('streaming')
-                var Hls = window.Hls
-                var url = '{{ config('app.streaming_play_url') }}'
-                if (Hls.isSupported()) {
-                    var hls = new Hls()
-                    hls.loadSource(url)
-                    hls.attachMedia(video_streaming)
-                } else if (video_streaming.canPlayType('application/vnd.apple.mpegurl')) {
-                    video_streaming.src = url
-                }
-            </script>
-        @endif
-        <div class="mdui-typo">
-            <span class="mdui-typo-headline">嗨, {{ Auth::user()->name }}。</span>
-            <br />
-            <span class="mdui-typo-headline-opacity hitokoto_text"></span>
+@endif
+<div class="mdui-typo">
+    <span class="mdui-typo-headline">嗨, {{ Auth::user()->name }}。</span>
+    <br />
+    <span class="mdui-typo-headline-opacity hitokoto_text"></span>
 
-            <form method="POST" action="{{ route('status.store') }}">
-                @csrf
-                <div class="mdui-textfield">
-                    <textarea class="mdui-textfield-input hitokoto_placeholder umami--input--status" name="content"
-                        maxlength="340" rows="4" required placeholder="I am because you are."></textarea>
-                </div>
-                <button class="mdui-btn mdui-color-theme mdui-ripple umami--click--publish-status">发布</button>
-            </form>
-
-            <div class="mdui-m-l-1 mdui-m-r-1">
-                <h4><a href="{{ route('main') }}"
-                        class="umami--click--user-toggle-timeriver">我的时间河</a>&nbsp;|&nbsp;<span class="global-time-river">全站时间河</span>&nbsp;|&nbsp;<a
-                        href="{{ route('articles') }}">博文</a></h4>
-                @include('include._feed')
-            </div>
-
+    <form method="POST" action="{{ route('status.store') }}">
+        @csrf
+        <div class="mdui-textfield">
+            <textarea class="mdui-textfield-input hitokoto_placeholder umami--input--status" name="content"
+                maxlength="340" rows="4" required placeholder="I am because you are."></textarea>
         </div>
-    @else
-        <div class="mdui-m-l-1 mdui-m-r-1 mdui-typo">
-            @include('include._feed')
-        </div>
-    @endauth
+        <button class="mdui-btn mdui-color-theme mdui-ripple umami--click--publish-status">发布</button>
+    </form>
 
-    <script>
-        window.util.text.putLyric(function(data) {
+    <div class="mdui-m-l-1 mdui-m-r-1">
+        <h4><a href="{{ route('main') }}" class="umami--click--user-toggle-timeriver">我的时间河</a>&nbsp;|&nbsp;<span
+                class="global-time-river">全站时间河</span>&nbsp;|&nbsp;<a href="{{ route('articles') }}">博文</a></h4>
+        @include('include._feed')
+    </div>
+
+</div>
+@else
+<div class="mdui-m-l-1 mdui-m-r-1 mdui-typo">
+    @include('include._feed')
+</div>
+@endauth
+
+<script>
+    window.util.text.putLyric(function(data) {
             $('.hitokoto_text').text(data.content)
         })
-    </script>
+</script>
 @endsection
--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -8,7 +8,9 @@
 <div class="mdui-row">
     <div class="mdui-col-xs-12 mdui-col-sm-8" id="streaming_div">
         <div class="mdui-typo-headline">{{ $live->name }}</div>
-        <video id="streaming" style="border-radius:5px;margin-top:10px" controls muted autoplay playsinline></video>
+        <video id="streaming" style="border-radius:5px;margin-top:10px" controls muted autoplay playsinline>
+            <source src="{{ config('app.streaming_play_url') }}" />
+        </video>
     </div>
     <div class="mdui-col-xs-12 mdui-col-sm-4">
         <ul id="comments">
@@ -17,18 +19,6 @@
     </div>
 </div>
 
-<script>
-    var video_streaming = document.getElementById('streaming')
-        var Hls = window.Hls
-        var url = '{{ config('app.streaming_play_url') }}'
-        if (Hls.isSupported()) {
-            var hls = new Hls()
-            hls.loadSource(url)
-            hls.attachMedia(video_streaming)
-        } else if (video_streaming.canPlayType('application/vnd.apple.mpegurl')) {
-            video_streaming.src = url
-        }
-</script>
 @else
 
 <div class="mdui-typo-headline mdui-typo">暂时没有流媒体安排，你可以去<a href="{{ route('live.create') }}">安排</a>一个。</div>
