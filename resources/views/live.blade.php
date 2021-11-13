27f5e770c0de649ec3c1c725c414213fbf5ab3c0
iVampireSP.com <im@ivampiresp.com>
2021-11-13 17:30:52 +0800

    增加 串流页面

---
+++ resources/views/live.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('title', '博文')
+
+@section('content')
+
+@if (!is_null($live))
+<div id="streaming_div">
+    <div class="mdui-typo-headline">{{ $live->name }}</div>
+
+    <video id="streaming" style="width:100%;border-radius:10px" controls></video>
+</div>
+
+<script>
+    var video_streaming = document.getElementById('streaming')
+        var Hls = window.Hls
+        var url =
+            '//{{ config('app.domain') }}/streaming/aeTimeRiver.m3u8'
+        if (Hls.isSupported()) {
+            var hls = new Hls()
+            hls.loadSource(url)
+            hls.attachMedia(video_streaming)
+            hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                video_streaming.play()
+            })
+        } else if (video_streaming.canPlayType('application/vnd.apple.mpegurl')) {
+            video_streaming.src = url
+            video_streaming.addEventListener('canplay', function() {
+                video_streaming.play()
+            })
+        }
+</script>
+@endif
+
+
+@endsection
