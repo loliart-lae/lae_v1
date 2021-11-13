37ece05e707c4ee8d008b83b255062e42cb37a8b
iVampireSP.com <im@ivampiresp.com>
2021-11-13 17:07:20 +0800

    改进 正在直播时的配色

--- resources/views/live/index.blade.php
+++ resources/views/live/index.blade.php
@@ -3,46 +3,42 @@
 @section('title', '流媒体节目表')
 
 @section('content')
-    <div class="mdui-typo-display-1">流媒体节目表</div>
+<div class="mdui-typo-display-1">流媒体节目表</div>
 
-    <div class="mdui-m-t-1">
-        <a href="{{ route('live.create') }}"
-            class="mdui-btn mdui-color-theme-accent mdui-ripple umami--click--goto-create-live">新建 节目安排</a>
-    </div>
+<div class="mdui-m-t-1">
+    <a href="{{ route('live.create') }}"
+        class="mdui-btn mdui-color-theme-accent mdui-ripple umami--click--goto-create-live">新建 节目安排</a>
+</div>
 
-    <div class="mdui-m-t-3">
-        <div class="mdui-table-fluid">
-            <table class="mdui-table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>名称</th>
-                        <th>开始时间</th>
-                        <th>结束时间</th>
-                        <th class="mdui-table-col-numeric">-</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @php($i = 0)
-                    @foreach ($lives as $live)
-                        <tr @if ($live->status) class="mdui-color-green-a700" @endif>
-                            <td>{{ ++$i }}</td>
-                            <td>{{ $live->name }}</td>
-                            <td>{{ $live->start_at }}</td>
-                            <td>{{ $live->end_at }}</td>
-
-                            <td class="mdui-typo">
-                                @if ((new \Illuminate\Support\Carbon)->diffInMinutes((new \Illuminate\Support\Carbon)->parse($live->start_at), false) < 0)
-                                    -
+<div class="mdui-m-t-3">
+    <div class="mdui-table-fluid">
+        <table class="mdui-table mdui-typo">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>名称</th>
+                    <th>开始时间</th>
+                    <th>结束时间</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php($i = 0)
+                @foreach ($lives as $live)
+                <tr @if ($live->status) style="background-color: #90f1a754!important;color:white" @endif>
+                    <td>{{ ++$i }}</td>
+                    <td><a href="
+                                 @if ((new \Illuminate\Support\Carbon)->diffInMinutes((new \Illuminate\Support\Carbon)->parse($live->start_at), false) < 0)
+                                    #
                                 @elseif ($live->user_id == Auth::id())
-                                    <a href="{{ route('live.edit', $live->id) }}">修改</a>
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                                   {{ route('live.edit', $live->id) }}
+                                @endif">{{ $live->name }}</td>
+                    <td>{{ $live->start_at }}</td>
+                    <td>{{ $live->end_at }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
+</div>
 
 @endsection
