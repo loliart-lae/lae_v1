c5977707d22aefad9fa027362125deff88994077
iVampireSP.com <im@ivampiresp.com>
2021-11-13 17:50:47 +0800

    移除 间隙

--- resources/views/include/_feed.blade.php
+++ resources/views/include/_feed.blade.php
@@ -1,64 +1,64 @@
 @guest
-    <div class="mdui-typo-display-1">时间长河</div>
+<div class="mdui-typo-display-1">时间长河</div>
 @endguest
 
 @if ($feed_items->count() > 0)
-    <div id="masonry" class="mdui-row">
-        @php($admins = config('admin.admin_users'))
+<div id="masonry" class="mdui-row">
+    @php($admins = config('admin.admin_users'))
 
-        @foreach ($feed_items as $status)
-            <div class="poll mdui-col-sm-4 mdui-col-xs-12 mdui-m-t-1">
-                <div class="mdui-card mdui-hoverable user_{{ $status->user->id }}_status" style="margin-top: 5px">
-                    <div class="mdui-card-header">
-                        <img class="mdui-card-header-avatar"
-                            src="{{ config('app.gravatar_url') }}/{{ md5(strtolower($status->user->email)) }}" />
-                        <div class="mdui-card-header-title">
-                            @if (is_null($status->user->website))
-                                {{ $status->user->name }}
-                            @else
-                                <a target="_blank" href="{{ $status->user->website }}">{{ $status->user->name }}</a>
-                            @endif
-                            <small> /
-                                {{ $status->created_at->diffForHumans() }}</small>
-                            <div style="display: inline;
+    @foreach ($feed_items as $status)
+    <div class="poll mdui-col-sm-4 mdui-col-xs-12 mdui-m-t-1">
+        <div class="mdui-card mdui-hoverable user_{{ $status->user->id }}_status" style="margin-top: 5px">
+            <div class="mdui-card-header">
+                <img class="mdui-card-header-avatar"
+                    src="{{ config('app.gravatar_url') }}/{{ md5(strtolower($status->user->email)) }}" />
+                <div class="mdui-card-header-title">
+                    @if (is_null($status->user->website))
+                    {{ $status->user->name }}
+                    @else
+                    <a target="_blank" href="{{ $status->user->website }}">{{ $status->user->name }}</a>
+                    @endif
+                    <small> /
+                        {{ $status->created_at->diffForHumans() }}</small>
+                    <div style="display: inline;
                                 position: absolute;
                                 right: 16px;
                                 margin-top: 3px;cursor: pointer">
-                                @if (in_array($status->user->email, $admins))
-                                    <span mdui-tooltip="{content: '官方人员'}"
-                                        class="mdui-icon material-icons-outlined material-icons-outlined verified_user">
-                                        verified_user
-                                    </span>
-                                @endif
-                                @auth
-                                    <span class="follow_{{ $status->user->id }}">
-                                        @if ($display ?? '' != 0)
-                                            @if ($status->user->id == Auth::id())
-                                                <i mdui-tooltip="{content: '这是你'}"
-                                                    class="mdui-text-color-theme mdui-icon material-icons-outlined"
-                                                    onclick="$(this).addClass('animate__animated animate__tada')">account_circle</i>
-                                            @elseif (in_array($status->user->id, $ids))
-                                                <i mdui-tooltip="{content: '已关注'}"
-                                                    onclick="$(this).addClass('animate__animated animate__pulse animate__infinite');toggleFollow({{ $status->user->id }})"
-                                                    class="mdui-text-color-theme mdui-icon material-icons-outlined umami--click--unfollow-user">favorite</i>
-                                            @else
-                                                <i mdui-tooltip="{content: '关注'}"
-                                                    onclick="$(this).addClass('animate__animated animate__pulse animate__infinite');toggleFollow({{ $status->user->id }})"
-                                                    class="mdui-text-color-black-secondary mdui-icon material-icons-outlined umami--click--follow-user">favorite</i>
-                                            @endif
-                                        @endif
-                                    </span>
-                                @endauth
-                            </div>
-                        </div>
-                        <div class="mdui-card-header-subtitle">{{ $status->user->bio ?? null }}</div>
+                        @if (in_array($status->user->email, $admins))
+                        <span mdui-tooltip="{content: '官方人员'}"
+                            class="mdui-icon material-icons-outlined material-icons-outlined verified_user">
+                            verified_user
+                        </span>
+                        @endif
+                        @auth
+                        <span class="follow_{{ $status->user->id }}">
+                            @if ($display ?? '' != 0)
+                            @if ($status->user->id == Auth::id())
+                            <i mdui-tooltip="{content: '这是你'}"
+                                class="mdui-text-color-theme mdui-icon material-icons-outlined"
+                                onclick="$(this).addClass('animate__animated animate__tada')">account_circle</i>
+                            @elseif (in_array($status->user->id, $ids))
+                            <i mdui-tooltip="{content: '已关注'}"
+                                onclick="$(this).addClass('animate__animated animate__pulse animate__infinite');toggleFollow({{ $status->user->id }})"
+                                class="mdui-text-color-theme mdui-icon material-icons-outlined umami--click--unfollow-user">favorite</i>
+                            @else
+                            <i mdui-tooltip="{content: '关注'}"
+                                onclick="$(this).addClass('animate__animated animate__pulse animate__infinite');toggleFollow({{ $status->user->id }})"
+                                class="mdui-text-color-black-secondary mdui-icon material-icons-outlined umami--click--follow-user">favorite</i>
+                            @endif
+                            @endif
+                        </span>
+                        @endauth
                     </div>
-                    <div class="mdui-card-content mdui-p-t-1">
-                        <div id="log_{{ $status->id }}"></div>
-                        <textarea id="log_{{ $status->id }}_content"
-                            style="display:none;">{!! e($status->content) !!}</textarea>
-                        <script>
-                            $(function() {
+                </div>
+                <div class="mdui-card-header-subtitle">{{ $status->user->bio ?? null }}</div>
+            </div>
+            <div class="mdui-card-content mdui-p-t-1">
+                <div id="log_{{ $status->id }}"></div>
+                <textarea id="log_{{ $status->id }}_content"
+                    style="display:none;">{!! e($status->content) !!}</textarea>
+                <script>
+                    $(function() {
                                 var log_view
                                 $('#log_{{ $status->id }}').html(null)
                                 log_view = editormd.markdownToHTML("log_{{ $status->id }}", {
@@ -68,39 +68,38 @@
                                     taskList: true,
                                 })
                             })
-                        </script>
-                    </div>
-                    <div class="mdui-card-actions">
-                        @auth
-                            <button id="status_{{ $status->id }}" onclick="toggleLike({{ $status->id }})"
-                                class="mdui-btn mdui-ripple mdui-btn-icon">
-                                @if (is_null($status->like))
-                                    <i class="mdui-icon material-icons-outlined umami--click--like"
-                                        style="color: unset">star_border</i>
-                                @elseif ($status->like->is_liked)
-                                    <i style="color:#36a6e8"
-                                        class="mdui-icon material-icons-outlined umami--click--unlike">star</i>
-                                @else
-                                    <i class="mdui-icon material-icons-outlined" style="color: unset">star_border</i>
-                                @endif
-                            </button>
-                        @endauth
-                    <a href="@auth {{ route('status.show', $status->id) }} @else {{ route('timeRiver.show', $status->id) }}
-                @endauth" class="mdui-btn mdui-ripple">@php($replies = count($status->replies)) @if ($replies > 0) {{ $replies }}条 @else 没有 @endif
-                        回复</a>
-                    @auth
-                        @can('destroy', $status)
-                            <form style="display: initial;" action="{{ route('status.destroy', $status->id) }}"
-                                method="POST" onsubmit="return confirm('确定要删除吗？删除后动态将会永远被埋没到长河中。');">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="mdui-btn mdui-ripple umami--click--status-delete">删除</button>
-                            </form>
-                        @endcan
-                    @endauth
-                </div>
+                </script>
+            </div>
+            <div class="mdui-card-actions">
+                @auth
+                <button id="status_{{ $status->id }}" onclick="toggleLike({{ $status->id }})"
+                    class="mdui-btn mdui-ripple mdui-btn-icon">
+                    @if (is_null($status->like))
+                    <i class="mdui-icon material-icons-outlined umami--click--like" style="color: unset">star_border</i>
+                    @elseif ($status->like->is_liked)
+                    <i style="color:#36a6e8" class="mdui-icon material-icons-outlined umami--click--unlike">star</i>
+                    @else
+                    <i class="mdui-icon material-icons-outlined" style="color: unset">star_border</i>
+                    @endif
+                </button>
+                @endauth
+                <a href="@auth{{ route('status.show', $status->id) }}@else{{ route('timeRiver.show', $status->id) }}@endauth"
+                    class="mdui-btn mdui-ripple">@php($replies = count($status->replies)) @if ($replies > 0) {{ $replies
+                    }}条 @else 没有 @endif
+                    回复</a>
+                @auth
+                @can('destroy', $status)
+                <form style="display: initial;" action="{{ route('status.destroy', $status->id) }}" method="POST"
+                    onsubmit="return confirm('确定要删除吗？删除后动态将会永远被埋没到长河中。');">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="mdui-btn mdui-ripple umami--click--status-delete">删除</button>
+                </form>
+                @endcan
+                @endauth
             </div>
         </div>
+    </div>
     @endforeach
 </div>
 <script>
