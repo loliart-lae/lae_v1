6fa09227048110df91467035b066b9d057672c79
iVampireSP.com <im@ivampiresp.com>
2021-11-14 21:17:32 +0800

    分开 API地址和支付地址

--- config/billing.php
+++ config/billing.php
@@ -4,6 +4,7 @@ return [
     'mid' => env('EPAY_MID'),
     'key' => env('EPAY_KEY'),
     'api_url' => env('EPAY_API_URL'),
+    'pay_method' => env('EPAY_API_PAY_METHOD'),
     'notify' => env('EPAY_NOFITY'),
     'return' => env('EPAY_RETURN'),
     'exchange_rate' => env('EPAY_EXCHANGE_RATE')
