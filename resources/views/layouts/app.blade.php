f178d6d9e60c225982c090b71746258085cc7e1b
iVampireSP.com <im@ivampiresp.com>
2021-11-17 14:10:08 +0800

    移除 底部LAE信息

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,19 +83,7 @@
         </div>
     </div>
 
-    <div class="mdui-container mdui-m-b-5 mdui-p-t-5 load-hidden">
-        <div class="mdui-typo">
-            <p class="mdui-typo-caption-opacity mdui-text-center">
-                {{ config('app.name') }}, <a class="umami--click--lae-github-repo"
-                    href="https://github.com/loliart-lae/lae">Github</a><br />
-                Crafted with 💗 by <a class="umami--click--contributes"
-                    href="{{ route('contributes') }}">Contributors</a><br />
-                Powered by LoliArt & Yistars
-            </p>
-        </div>
-    </div>
-
-    <a href="" id="thisLink" style="display: none"></a>
+    <a href="#" id="thisLink" style="display: none"></a>
 
     <div class="mdui-fab-wrapper mdui-fab-hide accelerato" id="bottom-fab">
         <button class="mdui-fab mdui-ripple mdui-color-theme">
