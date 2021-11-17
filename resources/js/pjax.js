8284fee37981edd3911a9b234455497ece9658a2
iVampireSP.com <im@ivampiresp.com>
2021-11-17 14:04:43 +0800

    优化 转译

--- resources/js/pjax.js
+++ resources/js/pjax.js
@@ -77,17 +77,3 @@ $(window).scroll(function () {
     }
 
 })
-
-// Unicode 转义
-function c(str) {
-    str = str.replace(/(\\u)(\w{1,4})/gi, function ($0) {
-        return (String.fromCharCode(parseInt((escape($0).replace(/(%5Cu)(\w{1,4})/g, "$2")), 16)));
-    });
-    str = str.replace(/(&#x)(\w{1,4});/gi, function ($0) {
-        return String.fromCharCode(parseInt(escape($0).replace(/(%26%23x)(\w{1,4})(%3B)/g, "$2"), 16));
-    });
-    str = str.replace(/(&#)(\d{1,6});/gi, function ($0) {
-        return String.fromCharCode(parseInt(escape($0).replace(/(%26%23)(\d{1,6})(%3B)/g, "$2")));
-    });
-    return str;
-}
--- resources/js/util.js
+++ resources/js/util.js
@@ -45,10 +45,28 @@ window.util = {
                     func(data)
                 },
                 error: function () {
-                    data = { status: 0, content: null, from: null, created_at: null }
+                    data = {
+                        status: 0,
+                        content: null,
+                        from: null,
+                        created_at: null
+                    }
                     func(data)
                 }
             })
+        },
+        c: function c(str) {
+            str = str.replace(/(\\u)(\w{1,4})/gi, function ($0) {
+                return (String.fromCharCode(parseInt((escape($0).replace(/(%5Cu)(\w{1,4})/g, "$2")), 16)));
+            });
+            str = str.replace(/(&#x)(\w{1,4});/gi, function ($0) {
+                return String.fromCharCode(parseInt(escape($0).replace(/(%26%23x)(\w{1,4})(%3B)/g, "$2"), 16));
+            });
+            str = str.replace(/(&#)(\d{1,6});/gi, function ($0) {
+                return String.fromCharCode(parseInt(escape($0).replace(/(%26%23)(\d{1,6})(%3B)/g, "$2")));
+            });
+            return str;
         }
+
     }
 }
