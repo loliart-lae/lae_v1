c291d8813d62eacc06a4de7131dff01369259b5d
iVampireSP.com <im@ivampiresp.com>
2021-11-19 10:28:35 +0800

    深色模式下的卡片

--- public/js/pjax.js
+++ public/js/pjax.js
@@ -1,1 +1,92 @@
-(()=>{var t=$("#top-tab").height(),i=$("#top-space").height(),o=function(){var t=window.location.protocol+"//"+window.location.host+window.location.pathname;$("#main-list a[href='"+t+"']").length>0&&($("#main-list .mdui-list-item").removeClass("mdui-list-item-active"),$("#main-list a[href='"+t+"']").addClass("mdui-list-item-active"),$("#backMain").attr("href",t))},n=function(){"/"!=window.location.pathname?($("#top-tab").css("min-height",0),$("#top-tab").css("height",0),$("#top-space").css("min-height",0),$("#top-space").css("height",0)):($("#top-tab").css("min-height",t),$("#top-tab").css("height",t),$("#top-space").css("min-height",i),$("#top-space").css("height",i))};$.ajaxSetup({headers:{"X-CSRF-TOKEN":$('input[name="_token"]').val()}}),$.pjax.defaults.timeout=1500,window.addEventListener("online",close_offline_tip),window.addEventListener("offline",showOfflineTip),$(document).pjax("a",".pjax-container"),$(document).on("pjax:clicked",(function(){$("#turn").css("animation-play-state","running")})),$(document).on("pjax:timeout",(function(t){$("#main").css("opacity",0),t.preventDefault()})),$(document).on("pjax:complete",(function(t){o(),$("#main").css("height","auto"),$("#main").css("overflow","unset"),$("#main").css("opacity",1),$("#turn").css("animation-play-state","paused"),$("#thisLink").attr("href",window.location.href),n();var i=document.title;i=i.replace(" - "+$("#app-name").text(),""),$("#top-title").text(i),mdui.mutation()})),window.history&&window.history.pushState&&(window.onpopstate=function(){o()}),o();var e=$("#main").offset().top;$(window).scroll((function(){var t=document.documentElement.scrollTop||document.body.scrollTop;(t>=e?1:0)?$("#bottom-fab").removeClass("mdui-fab-hide"):$("#bottom-fab").addClass("mdui-fab-hide")})),n()})();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./resources/js/pjax.js ***!
+  \******************************/
+var top_tab_height = $('#top-tab').height();
+var top_space = $('#top-space').height();
+var mainMenu = {
+  update: function update() {
+    var url = window.location.protocol + '//' + window.location.host + window.location.pathname;
+
+    if ($("#main-list a[href='" + url + "']").length > 0) {
+      $('#main-list .mdui-list-item').removeClass('mdui-list-item-active');
+      $("#main-list a[href='" + url + "']").addClass('mdui-list-item-active');
+      $("#backMain").attr('href', url);
+    }
+  },
+  top_app: function top_app() {
+    if (window.location.pathname != '/') {
+      $('#top-tab').css('min-height', 0);
+      $('#top-tab').css('height', 0);
+      $('#top-space').css('min-height', 0);
+      $('#top-space').css('height', 0);
+    } else {
+      $('#top-tab').css('min-height', top_tab_height);
+      $('#top-tab').css('height', top_tab_height);
+      $('#top-space').css('min-height', top_space);
+      $('#top-space').css('height', top_space);
+    }
+  }
+};
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-TOKEN': $('input[name="_token"]').val()
+  }
+});
+$.pjax.defaults.timeout = 1500;
+window.addEventListener('online', close_offline_tip);
+window.addEventListener('offline', showOfflineTip);
+$(document).pjax('a', '.pjax-container');
+$(document).on('pjax:clicked', function () {
+  // $('#main').css('filter', 'blur(1px)')
+  $('#turn').css('animation-play-state', 'running');
+});
+$(document).on("pjax:timeout", function (event) {
+  $('#main').css('opacity', 0);
+  event.preventDefault();
+});
+$(document).on("pjax:complete", function (event) {
+  mainMenu.update(); // 转译
+  // $('.pjax-container').html(c($('.pjax-container').html()))
+
+  $('#main').css('height', 'auto');
+  $('#main').css('overflow', 'unset');
+  $('#main').css('opacity', 1); // $('#main').css('filter', 'unset')
+
+  $('#turn').css('animation-play-state', 'paused');
+  $('#thisLink').attr('href', window.location.href);
+  mainMenu.top_app();
+  var title = document.title;
+  title = title.replace(' - ' + $('#app-name').text(), '');
+  $('#top-title').text(title);
+  mdui.mutation();
+});
+
+if (window.history && window.history.pushState) {
+  window.onpopstate = function () {
+    mainMenu.update();
+  };
+}
+
+mainMenu.update();
+var main_offset_top = $('#main').offset().top;
+var bottom_fab_status = 0;
+$(window).scroll(function () {
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (scrollTop >= main_offset_top) {
+    bottom_fab_status = 1;
+  } else {
+    bottom_fab_status = 0;
+  }
+
+  if (bottom_fab_status) {
+    $('#bottom-fab').removeClass('mdui-fab-hide');
+  } else {
+    $('#bottom-fab').addClass('mdui-fab-hide');
+  }
+});
+mainMenu.top_app();
+/******/ })()
+;
--- public/js/util.js
+++ public/js/util.js
@@ -1,1 +1,84 @@
-window.util={time:{formatSeconds:function(n){var r=parseInt(n),e=0,t=0;r>60&&(e=parseInt(r/60),r=parseInt(r%60),e>60&&(t=parseInt(e/60),e=parseInt(e%60)));var a=parseInt(r)+"秒";return e>0&&(a=parseInt(e)+"分"+a),t>0&&(a=parseInt(t)+"小时"+a),a}},dialog:{confirm:function(n){mdui.confirm("你正在进入一个安全的页面，请确保你现在没有录制或者进行公开的流式媒体，否则您可能会泄漏重要信息（如用户名，密码等）",(function(){window.open(n)}))}},text:{putLyric:function(n){$.ajax({url:"/api/v1/_lyric",method:"GET",success:function(r){n(r)},error:function(){data={status:0,content:null,from:null,created_at:null},n(data)}})},c:function(n){return n=(n=(n=n.replace(/(\\u)(\w{1,4})/gi,(function(n){return String.fromCharCode(parseInt(escape(n).replace(/(%5Cu)(\w{1,4})/g,"$2"),16))}))).replace(/(&#x)(\w{1,4});/gi,(function(n){return String.fromCharCode(parseInt(escape(n).replace(/(%26%23x)(\w{1,4})(%3B)/g,"$2"),16))}))).replace(/(&#)(\d{1,6});/gi,(function(n){return String.fromCharCode(parseInt(escape(n).replace(/(%26%23)(\d{1,6})(%3B)/g,"$2")))}))}}};
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./resources/js/util.js ***!
+  \******************************/
+window.util = {
+  time: {
+    formatSeconds: function formatSeconds(value) {
+      var secondTime = parseInt(value); // 秒
+
+      var minuteTime = 0; // 分
+
+      var hourTime = 0; // 小时
+
+      if (secondTime > 60) {
+        //如果秒数大于60，将秒数转换成整数
+        //获取分钟，除以60取整数，得到整数分钟
+        minuteTime = parseInt(secondTime / 60); //获取秒数，秒数取佘，得到整数秒数
+
+        secondTime = parseInt(secondTime % 60); //如果分钟大于60，将分钟转换成小时
+
+        if (minuteTime > 60) {
+          //获取小时，获取分钟除以60，得到整数小时
+          hourTime = parseInt(minuteTime / 60); //获取小时后取佘的分，获取分钟除以60取佘的分
+
+          minuteTime = parseInt(minuteTime % 60);
+        }
+      }
+
+      var result = "" + parseInt(secondTime) + "秒";
+
+      if (minuteTime > 0) {
+        result = "" + parseInt(minuteTime) + "分" + result;
+      }
+
+      if (hourTime > 0) {
+        result = "" + parseInt(hourTime) + "小时" + result;
+      }
+
+      return result;
+    }
+  },
+  dialog: {
+    confirm: function confirm(url) {
+      mdui.confirm('你正在进入一个安全的页面，请确保你现在没有录制或者进行公开的流式媒体，否则您可能会泄漏重要信息（如用户名，密码等）', function () {
+        window.open(url);
+      });
+    }
+  },
+  text: {
+    putLyric: function putLyric(func) {
+      $.ajax({
+        url: '/api/v1/_lyric',
+        method: 'GET',
+        success: function success(data) {
+          func(data);
+        },
+        error: function error() {
+          data = {
+            status: 0,
+            content: null,
+            from: null,
+            created_at: null
+          };
+          func(data);
+        }
+      });
+    },
+    c: function c(str) {
+      str = str.replace(/(\\u)(\w{1,4})/gi, function ($0) {
+        return String.fromCharCode(parseInt(escape($0).replace(/(%5Cu)(\w{1,4})/g, "$2"), 16));
+      });
+      str = str.replace(/(&#x)(\w{1,4});/gi, function ($0) {
+        return String.fromCharCode(parseInt(escape($0).replace(/(%26%23x)(\w{1,4})(%3B)/g, "$2"), 16));
+      });
+      str = str.replace(/(&#)(\d{1,6});/gi, function ($0) {
+        return String.fromCharCode(parseInt(escape($0).replace(/(%26%23)(\d{1,6})(%3B)/g, "$2")));
+      });
+      return str;
+    }
+  }
+};
+/******/ })()
+;
